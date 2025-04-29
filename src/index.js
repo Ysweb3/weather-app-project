514652cba4f398a7e9eff773c1bf685ceee23483
Ysweb3 <yssh88972@gmail.com>
2025-04-29 16:31:51 +0530

    Create fetch function for weather api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./styles.css";
 
+async function getWeather(value){
+    try{
+        const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+value+"/?key=B2TNXK8VZYBRPC8W7DCV9LYCA")
+        const weatherData = await response.json();
+        return weatherData;
+    }
+    catch (error){
+        console.log("Invalid City " )
+    }
+}
+// getWeather("india")
+// .then(temp =>{
+//     console.log(temp.currentConditions);
+// })
+// .catch(error =>{
+//     console.log(error);
+// });
 
-console.log("Hello World!");
+console.log(getWeather("indi"))
